56cde1f0b7e46f75338bbfb2383e0f17de15ffee
salinas-manuel <salinas.manuel_1980@yahoo.com>
2018-02-19 10:38:37 -0600

    Displaying card in homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { HttpClientModule } from '@angular/common/http';
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
+import { Platform } from 'ionic-angular';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { CardSearchPage } from '../pages/card-search/card-search';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { CardsProvider } from '../providers/cards/cards';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    CardSearchPage
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    CardSearchPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ScreenOrientation,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CardsProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CardsProvider } from '../../providers/cards/cards';
+import { Cards } from '../../models/cards';
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
+import { Platform } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +12,45 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public card: Cards;
 
+  constructor(
+    public navCtrl: NavController,
+    private cardsProvider: CardsProvider,
+    private screenOrientation: ScreenOrientation,
+    public plt: Platform
+    
+  ){
+    this.plt.ready().then((readySource) => {
+      console.log('Platform ready from', readySource);
+      console.log(this.screenOrientation.type);
+        if (this.plt.is('android')){
+          console.log("This is Android");
+          //this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
+        }
+    });
   }
 
+  public ionViewDidLoad() {
+    //this.screenOrientation.lock('portrait');
+    
+    this.getCard();
+    //this.screenLock();
+  }
+
+  /*public screenLock(){
+    if(this.plt.is('cordova')){
+      console.log(this.screenOrientation.type);
+    }
+  }*/
+  
+  public getCard(){
+    this.cardsProvider.getCard().subscribe(
+      (response) => {
+        console.log(response);
+        this.card = response;
+ 
+      }
+    )
+  }
 }
